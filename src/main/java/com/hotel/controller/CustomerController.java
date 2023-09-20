1e56951484b92e9a3a5c81c647fe1ba8f6379839
khushvant <khusujadhao329@gmail.com>
2023-09-20 14:11:06 +0530

    Add CustomerController.java

---
+++ src/main/java/com/hotel/controller/CustomerController.java
@@ -0,0 +1,44 @@
+package com.hotel.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hotel.entity.Customer;
+import com.hotel.service.CustomerService;
+
+@RestController
+@RequestMapping("/customer/api")
+public class CustomerController {
+	
+	@Autowired
+	CustomerService customerService;
+	@GetMapping
+	public ResponseEntity<List<Customer>>getAllcustomer(){
+		List<Customer>customers=customerService.getAllCustomers();
+		
+		return new ResponseEntity<>(customers,HttpStatus.OK);
+		
+	}
+	@PostMapping
+	public ResponseEntity<Customer>createCustomer(@RequestBody Customer customer){
+		Customer customer2=customerService.createCustomer(customer);
+		return new ResponseEntity<Customer>(customer2,HttpStatus.CREATED);
+		
+	}
+	@GetMapping("/{Id}")
+	public ResponseEntity<Customer>findCustomerById(@PathVariable Long Id) throws Exception{
+		Customer customer=customerService.findCustomerById(Id);
+		return new ResponseEntity<>(customer,HttpStatus.OK);
+		
+	}
+
+}
