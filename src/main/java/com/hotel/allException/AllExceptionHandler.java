bf036072d9d862e0f9a74fab001e5823aefcb375
khushvant <khusujadhao329@gmail.com>
2023-09-20 14:08:07 +0530

    Add AllExceptionHandler.java

---
+++ src/main/java/com/hotel/allException/AllExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.hotel.allException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class AllExceptionHandler {
+	@ExceptionHandler(value = NotFondException.class)
+	public ResponseEntity<String>NotFondExceptionHandler(NotFondException notFondException){
+		return new ResponseEntity<String>(notFondException.getLocalizedMessage(),HttpStatus.NOT_FOUND);
+		
+	}
+
+}
