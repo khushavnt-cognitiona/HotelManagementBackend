452ff3d7a095c6237ece3dc551b06e3566f99252
khushvant <khusujadhao329@gmail.com>
2023-09-20 14:26:28 +0530

    Add delete endpoint in CustomerController

--- src/main/java/com/hotel/controller/CustomerController.java
+++ src/main/java/com/hotel/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,5 +41,13 @@ public class CustomerController {
 		return new ResponseEntity<>(customer,HttpStatus.OK);
 		
 	}
+	
+	//delete customer delete bye id
+	@DeleteMapping("delete/{Id}")
+	public void deleteCustomerById  (@PathVariable Long Id) {
+		customerService.deleteCustomerById(Id);
+		
+	}
+	
 
 }
